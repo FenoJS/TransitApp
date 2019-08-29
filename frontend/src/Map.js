df73adfa05972e12193fa15653a177dd63fb17fe
FenoJS <michalskiJS@gmail.com>
2019-08-29 08:27:46 +0200

    update map component with basic settings

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import L from 'leaflet';
 
 const Map = () => {
-  return (
-    <div>
-      MAP
-    </div>
-  );
+  const style = {
+    height: '100vh',
+    width: '100%',
+  };
+
+  useEffect(() => {
+    const mymap = L.map('mapid').setView([52.229675, 21.01223], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+      maxZoom: 18,
+    }).addTo(mymap);
+  }, []);
+
+  return <div id="mapid" style={style}></div>;
 };
 
 export default Map;
