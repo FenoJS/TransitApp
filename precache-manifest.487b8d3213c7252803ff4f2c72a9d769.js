3b976edf948eaf76932620cc7ca22a31807ab2c5
FenoJS <michalskiJS@gmail.com>
2019-12-09 07:06:33 +0100

    Updates

---
+++ precache-manifest.487b8d3213c7252803ff4f2c72a9d769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9340d30b6f24a3d55012cc0ba1d68863",
+    "url": "/TransitApp/index.html"
+  },
+  {
+    "revision": "2bacc922c1469a2a2afb",
+    "url": "/TransitApp/static/css/main.8d31e13e.chunk.css"
+  },
+  {
+    "revision": "9388f2495641ed2ad234",
+    "url": "/TransitApp/static/js/2.2dc5f7f5.chunk.js"
+  },
+  {
+    "revision": "2bacc922c1469a2a2afb",
+    "url": "/TransitApp/static/js/main.372073de.chunk.js"
+  },
+  {
+    "revision": "38006fe914822f49fb71",
+    "url": "/TransitApp/static/js/runtime~main.f5c548c3.js"
+  },
+  {
+    "revision": "16762100822871c7830498ea08623450",
+    "url": "/TransitApp/static/media/guestAvatar.16762100.svg"
+  }
+]);
