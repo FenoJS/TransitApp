efdaf7c6adcf63ec74c4e884242b89da281ed58b
FenoJS <michalskiJS@gmail.com>
2021-03-18 12:39:21 +0100

    change API url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -55,7 +55,7 @@ class App extends Component {
         isLoading: true,
       });
       const data = await fetch(
-        `https://cors-anywhere.herokuapp.com/http://8.209.115.48:8080/otp/routers/default/plan?fromPlace=${startCords}&toPlace=${goalCords}&date=2019-08-26&time=${currentTime}`
+        `http://8.209.115.48:8080/otp/routers/default/plan?fromPlace=${startCords}&toPlace=${goalCords}&date=2019-08-26&time=${currentTime}`
       ).then((res) => res.json());
 
       const routesData = await data.plan.itineraries;
